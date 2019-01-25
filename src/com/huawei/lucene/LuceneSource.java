068eda702c60e97c8cc202c465bfc336e06ef22b
ThinkPad <ThinkPad@DESKTOP-DCF1CUI>
2019-01-25 17:38:55 +0800

    ok

--- src/com/huawei/lucene/LuceneSource.java
+++ src/com/huawei/lucene/LuceneSource.java
@@ -7,7 +7,6 @@ import java.util.TimerTask;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.IndexableField;
-import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
